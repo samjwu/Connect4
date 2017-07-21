40d52933b0eb442ddcd92e2086cafb2fe92473b0
Sam Wu <sjwu@ualberta.ca>
2017-07-20 20:50:27 -0700

    add key variables

---
+++ gameplayconstants.js
@@ -0,0 +1,30 @@
+/*
+Contain the gameplay constants and variables for game
+*/
+
+//UI object to hold names
+//https://stackoverflow.com/questions/418799/what-does-colon-do-in-javascript
+var UI = {
+    player1name: "Player 1", //red
+    player2name: "Player 2", //yellow
+    startingplayer: "red",
+    turnmessage: "Player to move: ",
+    invalidmovemessage: "Invalid move. Choose another coordinate.",
+    drawmessage: "Draw!",
+    winmessage: "Winner: ",
+    wincondition: 4, //need this many chips in a row to win
+    };
+
+//Set Player 1 as starting player to move
+var playertomove = UI.startingplayer;
+
+/*
+define board as 2D array
+0 represents empty
+*/
+var board = [[0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0],
+            [0,0,0,0,0,0,0]];
