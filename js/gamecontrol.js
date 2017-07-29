775821bdaa3200a3176b0145a0b9c4406c5265c4
Sam Wu <sjwu@ualberta.ca>
2017-07-28 22:51:25 -0700

    controls for buttons

--- js/gamecontrol.js
+++ js/gamecontrol.js
@@ -3,20 +3,22 @@ Contain code for player to interact with DOM
 Uses game logic functions to allow player to play game
 */
 
+//Run js once DOM (HTML page) is loaded
 //https://www.w3schools.com/js/js_syntax.asp
 //https://stackoverflow.com/questions/1150381/what-is-the-meaning-of-sign-in-javascript
 //https://learn.jquery.com/using-jquery-core/dollar-object-vs-function/
 //https://learn.jquery.com/using-jquery-core/document-ready/
 $(document).ready(function() {
 
+    //Get player names
     //https://www.w3schools.com/jsref/met_win_prompt.asp
     UI.player1name = prompt("Enter Player 1 Name:", UI.player1name);
     UI.player2name = prompt("Enter Player 2 Name:", UI.player2name);
 
-    //var for turn messages
+    //Variable for turn messages
     $(".playerui").text(UI.turnmessage); 
 
-    //add text for turn messages
+    //Add text for turn messages
     if (playertomove == "red") {
         $("playerui").removeClass(); //delete old text
         $("playerui").addClass(playertomove).text(UI["player2name"]); //add new text
@@ -26,5 +28,12 @@ $(document).ready(function() {
         $("playerui").removeClass();
         $("playerui").addClass(playertomove).text(UI["player1name"]);
     }
+
+    //Function for running game after button click on board
+    $(".board button").click(function(event) {
+        //Get position of button
+        var row = $('.board tr').index($(this).closest('tr')); //y
+        var col = $(this).closest('tr').find('td').index($(this).closest('td')); //x
+    });
 
 });
